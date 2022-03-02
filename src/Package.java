673587ae6c4597c3f960b5ff9246935d3d55928d
allenzph <zph13700225692@126.com>
2022-03-02 11:32:05 +0800

    practice the package type of java

--- src/Package.java
+++ src/Package.java
@@ -3,6 +3,21 @@ public class Package {
         Integar n = null;
         Integar n2 = new Integar(99);
         int n3 = n2.intValue();
+        /*java自带的包装类型
+        boolean java.lang.Boolean
+        byte    java.lang.Byte
+        short   java.lang.Short
+        int     java.lang.Integar
+        long    java.lang.Long
+        float   java.lang.Float
+        double  java.lang.Double
+        char    java.lang.Character
+         */
+        int i = 100;
+        Integer a1 = Integer.valueOf(i);
+        Integer a2 = Integer.valueOf("100");
+        System.out.println(a1.intValue());
+        System.out.println(a2.intValue());
     }
 }
 
