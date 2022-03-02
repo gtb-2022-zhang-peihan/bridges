64207451fff3af0cebb7af20bbce3f0863c6895c
allenzph <zph13700225692@126.com>
2022-03-02 11:18:28 +0800

    add integar file to practice

---
+++ src/Integar.java
@@ -0,0 +1,11 @@
+public class Integar {
+    private int value;
+
+    public Integar(int value) {
+        this.value = value;
+    }
+
+    public int intValue() {
+        return this.value;
+    }
+}
