f863e5aa29784e293b00347a3ceafa95f09ea39f
allenzph <zph13700225692@126.com>
2022-03-02 13:27:45 +0800

    practice .eauql() in boxing

--- src/Package.java
+++ src/Package.java
@@ -14,7 +14,7 @@ public class Package {
         char    java.lang.Character
          */
         int i = 100;
-        Integer a1 = Integer.valueOf(i);
+        Integer a1 = Integer.valueOf(i);//
         Integer a2 = Integer.valueOf("100");
         System.out.println(a1.intValue());
         System.out.println(a2.intValue());
@@ -23,6 +23,15 @@ public class Package {
         int r = q.intValue();
         int x = q;
         System.out.println(r);
+        //不变类 所有的包装类型都是不变类 进行比较时要有equal()而不是==
+        Integer q1 = 99;
+        Integer q2 = 99;
+        Integer q3 = 123465;
+        Integer q4 = 123465;
+        System.out.println("q1 == q2: " + (q1==q2));
+        System.out.println("q3 == q4: " + (q3==q4));//数值较小时系统会自动将 Integar x = 99 变为 Integar x = integar.Valueof(99),实际应用中不要用这种方法
+        System.out.println("q1.equal(q2): " + q1.equals(q2));
+        System.out.println("q3.equal(q4): " + q3.equals(q4));
     }
 }
 
