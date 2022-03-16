1db8f0317d45402c3895ef857d7e90e62a7b83e1
allenzph <zph13700225692@126.com>
2022-03-16 23:08:58 +0800

    practice at 2022.3.16

--- src/Allen.java
+++ src/Allen.java
@@ -1,40 +1,124 @@
 public class Allen {
     public static void main(String[] args) {
         Person z = new Person("zhang",25);
-        System.out.println(z.getFirstname());
+        System.out.println(z.getName());
         System.out.println(z.getAge());
         Person y = new Person("yang");
         System.out.println(y.getAge());
         HHello zh = new HHello();
         zh.hello("zhangpeihan");
         zh.hello("zhangyong",52);
+        Student q = new Student("aaa",15,85);
+        Books maoxuan = new Books();
+        maoxuan.setName("毛泽东选集");
+        Student q1 = new Student(maoxuan);
+        q1.reading();
+        Person w = new Student("ff");
+        Person w1 = new Person();
+        System.out.println(q.getScore());
+        System.out.println(q.getName());
+        Person xiaoming = new Person("Xiaoming",12);
+        Student xiaohong = new Student("Xiaohong",20,99);
+        Student xiaojun = new PrimaryStudent("Xiaojun",9,100,5);
+        System.out.println(xiaojun.getAge());
+        System.out.println(xiaojun.getScore());
+        System.out.println(xiaojun.getName());
+        //System.out.println(xiaojun.getGrade());
     }
 }
 
-class Person {
-    private String firstname = "unnamed";
-    private int age = 1;
 
-    public Person(String firstname) {
-        this(firstname,18);
+
+class Person {
+    protected String name = "unnamed";
+    protected int age = 1;
+
+    public Person(String name) {
+        this(name,18);
     }
 
     public Person() {
 
     }
 
-    public Person(String firstname, int age) {
-        this.firstname = firstname;
+    public Person(String name, int age) {
+        this.name = name;
         this.age = age;
     }
 
-    public String getFirstname() {
-        return this.firstname;
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getAge() {
         return this.age;
     }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+class Student extends Person {
+    protected Books books;
+    protected int score;
+
+    public Student(Books books) {
+        this.books = books;
+    }
+
+    public void reading(){
+        System.out.println(books.getName());
+    }
+
+    public Student(){
+        super();
+    }
+
+    public Student(String name){
+
+    }
+
+    public Student(String name, int age, int score) {
+        super(name,age);
+        this.score =score;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+}
+
+class Books {
+    protected String name;
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
+
+class PrimaryStudent extends Student {
+    protected int grade;
+
+    public PrimaryStudent(String name, int age, int score, int grade) {
+        super(name,age,score);
+        this.grade = grade;
+    }
+
+    public int getGrade() {
+        return this.grade;
+    }
+
 }
 
 class HHello {
