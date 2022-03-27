936655e5f491d063614eae7c83394132b7f57f3a
allenzph <zph13700225692@126.com>
2022-03-27 15:42:29 +0800

    CollectionPractice at 2022.3.27

---
+++ src/JiHe/CollectionPractice.java
@@ -0,0 +1,59 @@
+package JiHe;
+import java.util.*;
+;
+
+public class CollectionPractice {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("apple");
+        list.add("banana");
+        list.add("apple");
+        list.add(null);
+        System.out.println(list.size());
+        String apple = list.get(1);
+        System.out.println(apple);
+        List<Integer> list1 = List.of(1,55,66,88);
+        List<String> list2 = List.of("a","b","c","d");
+        for(int i = 0; i < list2.size(); i++) {
+            String s = list2.get(i);
+            System.out.println(s);
+        }
+        for(Iterator<String> it = list2.iterator(); it.hasNext();) {
+            String q = it.next();
+            System.out.println(q);
+        }
+        Integer [] array = list1.toArray(new Integer[list1.size()]);
+        for (Integer i : array) {
+            System.out.println(i);
+        }
+        String[] array1 = {"1","2","3"};
+        List<String> list3 = List.of(array1);
+        for (String n : array1) {
+            System.out.println(n);
+        }
+
+        Map<String, String> map = new HashMap< >();
+        map.put("a", new String("zhang"));
+        map.put("b", new String("wang"));
+        System.out.println(map.get("a"));
+        System.out.println(map.get("c"));
+
+        List<String> list4 = new ArrayList<>();
+        list4.add("qqq");
+        list4.add("aaa");
+        list4.add("sss");
+        System.out.println(list4);
+        Collections.sort(list4);
+        System.out.println(list4);
+
+        List<Integer> list5 = new ArrayList<>();
+        for (int i = 0 ; i < 10 ; i++) {
+            list5.add(i);
+        }
+        System.out.println(list5);
+        Collections.shuffle(list5);
+        System.out.println(list5);
+    }
+}
+
+
