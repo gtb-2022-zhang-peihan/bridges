ecd6bde106772e06e6f77aff84277045f1b2817d
allenzph <zph13700225692@126.com>
2022-03-29 21:43:26 +0800

    CollectionPractice at 2022.3.27

--- src/JiHe/CollectionPractice.java
+++ src/JiHe/CollectionPractice.java
@@ -53,6 +53,18 @@ public class CollectionPractice {
         System.out.println(list5);
         Collections.shuffle(list5);
         System.out.println(list5);
+
+        String text =
+                "A panda walks into a cafe. He orders a sandwich, eats it, then " +
+                        "draws a gun and fires two shots in the air." +
+                        "\"Why?\" asks the confused waiter, as the panda makes towards the exit." +
+                        "The panda produces a badly punctuated wildlife manual and tosses it over" +
+                        "his shoulder. \"I'm a panda,\" he says, at the door." +
+                        "\"Look it up.\" The waiter turns to the relevant entry and, sure enough, " +
+                        "finds an explanation..." +
+                        "\"Panda. Large black-and-white bear-like mammal, native to China. Eats, " +
+                        "shoots and leaves.\"";
+        System.out.println(text);
     }
 }
 
